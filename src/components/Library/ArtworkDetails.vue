1d4ff505de71b9521d79a34fc8e56d55f3526259
Amirhossein Al <amirh715@gmail.com>
2022-02-12 23:42:27 +0330

    Add ReleaseDateDisplayer

--- src/components/Library/ArtworkDetails.vue
+++ src/components/Library/ArtworkDetails.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="artwork && hasAtLeastOneProperty" class="flex">
+  <div v-if="artwork && hasAtLeastOneProperty" class="flex flex-column space-2">
     <p v-if="artwork.producer">
       تهیه کننده: {{artwork.producer}}
     </p>
@@ -7,7 +7,7 @@
       رکورد لیبل: {{artwork.recordLabel}}
     </p>
     <p v-if="artwork.releaseDate">
-      تاریخ انتشار: {{artwork.releaseDate}}
+      تاریخ انتشار: <release-date-displayer :value="artwork.releaseDate" />
     </p>
   </div>
   <div v-else class="flex">
@@ -20,8 +20,10 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 import { ArtworkDetailsDTO } from '@/classes/Library/query/ArtworkDetailsDTO'
+import ReleaseDateDisplayer from '../common/ReleaseDateDisplayer.vue';
 
 export default defineComponent({
+  components: { ReleaseDateDisplayer },
   name: 'artwork-details',
   props: {
     artwork: Object,
