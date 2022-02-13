7a287a30e588e030b0e30711643e7b4289db7b5c
Amirhossein Al <amirh715@gmail.com>
2022-02-13 21:17:46 +0330

    Recover NewReportScreen.vue
    This file was commited to a wrong state but was recovered

--- src/views/Report/NewReportScreen.vue
+++ src/views/Report/NewReportScreen.vue
@@ -1,10 +1,118 @@
 <template>
-  <div></div>
+  
+  <ion-content>
+    
+    <ion-toolbar>
+      <b>گزارش اشکال</b>
+    </ion-toolbar>
+
+    <ion-card>
+      <ion-card-content>
+        <div class="flex flex-column">
+          <div>
+            <p v-if="reportedEntityId">
+              شما در حال ثبت گزارش درباره <b>{{reportedEntityTitle}}</b>
+              هستید. اگر مشکلی که با آن مواجه شده اید در مورد هنرمند یا 
+              اثر خاصی نیست از طریق گزینه گزارش اشکال در صفحه پروفایل تان 
+              گزارش را ثبت کنید.
+            </p>
+            <p v-else>
+              لطفا اگر مشکلی که با آن مواجه شده اید در مورد یک هنرمند یا اثر خاص است، به صفحه آن هنرمند یا اثر رفته و 
+              از طریق گزینه <ion-icon :icon="bugOutline"></ion-icon>
+              در بالای صفحه گزارش را ثبت کنید.
+            </p>
+          </div>
+          <div class="flex justify-content-center align-items-center space-v">
+            <ion-label class="space-h">نوع گزارش</ion-label>
+            <ion-select
+              v-model="reportType"
+              cancel-text="بیخیال"
+              ok-text="برو"
+              title="نوع گزارش"
+              style="width: 8rem">
+                <ion-select-option value="TECHNICAL">
+                  <span>فنی</span>
+                </ion-select-option>
+                <ion-select-option value="CONTENT">
+                  <span>محتوایی</span>
+                </ion-select-option>
+            </ion-select>
+          </div>
+          <label class="space-v">متن گزارش</label>
+          <ion-textarea
+            v-model="message"
+            placeholder="لطفا مشکلی که با آن روبرو شده اید را دقیق و با جزئیات شرح دهید..."
+            rows="7"
+          />
+          <div>
+            <ion-button @click="submit">
+              <ion-icon :icon="sendOutline"></ion-icon>
+            </ion-button>
+          </div>
+        </div>
+      </ion-card-content>
+    </ion-card>
+
+    <ion-fab vertical="bottom" horizontal="start">
+      <ion-fab-button @click="goToMyReports">
+        <ion-icon :icon="listOutline"></ion-icon>
+      </ion-fab-button>
+    </ion-fab>
+
+  </ion-content>
+
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import { sendOutline, checkmarkCircleOutline, closeCircleOutline, bugOutline, listOutline } from 'ionicons/icons'
+import { ReportService } from '@/services/ReportService';
+import { CreateNewReportDTO } from '@/classes/Report/commands/CreateNewReportDTO';
+import { toastController, ToastOptions } from '@ionic/vue';
 
 export default defineComponent({
+  data() {
+    return {
+      reportType: '',
+      message: '',
+      reportedEntityId: this.$route.query.reportedEntityId,
+      reportedEntityTitle: this.$route.query.reportedEntityTitle,
+      sendOutline,
+      checkmarkCircleOutline,
+      closeCircleOutline,
+      bugOutline,
+      listOutline,
+    };
+  },
+  methods: {
+    async submit() {
+      await this.$router.push({ name: 'MyReports' });
+      let options: ToastOptions;
+      try {
+        const dto = new CreateNewReportDTO({
+          reportType: this.reportType,
+          message: this.message,
+        });
+        await ReportService.createNewReport(dto);
+        options = {
+          message: 'گزارش شما ثبت شد. ممنون.',
+          icon: checkmarkCircleOutline,
+          duration: 4000,
+        }
+        await this.$router.push({ name: 'MyReports' });
+      } catch(err) {
+        options = {
+          message: err.response.data.message,
+          icon: closeCircleOutline,
+          duration: 4000,
+        }
+      } finally {
+        await toastController.create(options);
+      }
+    },
+    goToMyReports() {
+      this.$router.push({ name: 'MyReports' });
+    },
+  },
 })
 </script>
