eecfba4f1e08c7dc49b4d118b6467fcd4c33bd40
Amirhossein Al <amirh715@gmail.com>
2022-02-14 01:43:39 +0330

    Implement DateTimeDisplayer

---
+++ src/components/common/DateTimeDisplayer.vue
@@ -0,0 +1,24 @@
+<template>
+  <number-displayer :value="displayValue" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import moment from 'moment-jalaali';
+import NumberDisplayer from './NumberDisplayer.vue';
+
+export default defineComponent({
+  components: { NumberDisplayer },
+  name: 'date-time-displayer',
+  props: {
+    value: Date,
+    format: String,
+  },
+  computed: {
+    displayValue() {
+      const format = this.format || ' HH:mm:ss jYYYY/jM/jD';
+      return moment(this.value).format(format);
+    },
+  },
+})
+</script>
