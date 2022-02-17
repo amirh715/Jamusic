57db096226d28102b6f556bc5bf2b0ffa87d1845
Amirhossein Al <amirh715@gmail.com>
2022-02-18 03:28:06 +0330

    Add route change animation

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,29 @@
 <template>
   <ion-app>
-    <!-- <ion-router-outlet /> -->
-    <router-view/>
+    <router-view />
     <ion-loading v-if="$store.state.isAppWaiting" message="منتظر باشید"></ion-loading>
   </ion-app>
 </template>
+
+<style scoped>
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .2s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
+.slide-fade-enter-active {
+  transition: all .2s ease;
+}
+.slide-fade-leave-active {
+  transition: all .2s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+}
+.slide-fade-enter, .slide-fade-leave-to
+/* .slide-fade-leave-active below version 2.1.8 */ {
+  transform: translateX(10px);
+  opacity: 0;
+}
+</style>
 
 <script lang="ts">
 import { IonApp, IonRouterOutlet } from '@ionic/vue';
@@ -15,6 +34,13 @@ export default defineComponent({
   components: {
     IonApp,
     // IonRouterOutlet
-  }
+  },
+  watch: {
+    '$route'(to, from) {
+      const toDepth = to.path.split('/').length;
+      const fromDepth = from.path.split('/').length;
+      this.transitionName = toDepth < fromDepth ? 'slide-fade' : 'slide-fade';
+    },
+  },
 });
 </script>
