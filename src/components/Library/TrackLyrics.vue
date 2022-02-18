026e29c5f4dd002b8c7b60d8bddd51339aae56eb
Amirhossein Al <amirh715@gmail.com>
2022-02-18 03:39:12 +0330

    Add Track lyrics component

---
+++ src/components/Library/TrackLyrics.vue
@@ -0,0 +1,16 @@
+<template>
+  <div style="max-height: 15rem; overflow-y: scroll">
+    {{lyrics}}
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'track-lyrics',
+  props: {
+    lyrics: String,
+  },
+})
+</script>
