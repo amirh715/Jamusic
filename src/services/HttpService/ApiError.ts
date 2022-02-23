470598e52ed16e4cdb7d3821726859acda56224f
Amirhossein Al <amirh715@gmail.com>
2022-02-24 02:37:03 +0330

    Add error type

--- src/services/HttpService/ApiError.ts
+++ src/services/HttpService/ApiError.ts
@@ -1,14 +1,17 @@
 class ApiError extends Error {
 
+  public type: string;
   public code: number;
   public description: string;
 
   public constructor(_props:{
+    type: string,
     message: string,
     code: number,
     description: string,
   }) {
     super(_props.message);
+    this.type = _props.type;
     this.code = _props.code;
     this.description = _props.description;
   }
