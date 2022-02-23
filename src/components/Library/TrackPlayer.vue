b991a5294582ef25c30bb119c42733327a0cc7ed
Amirhossein Al <amirh715@gmail.com>
2022-02-24 02:50:20 +0330

    Fix seeking problem
    ionChange would fire either when the user changed the seek position or when the track was playing which caused a sound problem.

--- src/components/Library/TrackPlayer.vue
+++ src/components/Library/TrackPlayer.vue
@@ -1,7 +1,23 @@
 <template>
   <div class="flex justify-content-around align-items-center">
-    <ion-range></ion-range>
+    <div style="width: 90%">
+      <ion-range
+        ref="range"
+        :value="currentSeekPosition"
+        @ionFocus="isRangeFocused = true"
+        @ionBlur="isRangeFocused = false"
+        @ionChange="seekPositionChanged"
+        min="0" max="100">
+          <ion-label slot="end" style="min-width: 45px;">
+            <duration-displayer :durationInSec="currentDuration" />
+          </ion-label>
+          <ion-label slot="start">
+            <duration-displayer :durationInSec="totalDuration" />
+          </ion-label>
+      </ion-range>
+    </div>
     <ion-icon
+      style="width: 10%;"
       size="large"
       :icon="playerIcon"
       @click="playPauseButtonClicked"
@@ -10,37 +26,61 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 import { playCircleOutline, pauseCircleOutline } from 'ionicons/icons'
 import { ACTION_TYPES } from '@/store/ACTION_TYPES';
+import { TrackDetailsDTO } from '@/classes/Library/query/TrackDetailsDTO';
 
 export default defineComponent({
   name: 'track-player',
   props: {
-    trackId: String,
+    track: TrackDetailsDTO,
   },
   data() {
     return {
-      currentDuration: 0,
-      totalDuration: 0,
+      isRangeFocused: false,
       playCircleOutline,
       pauseCircleOutline,
     };
   },
   computed: {
+    currentDuration() {
+      return this.$store.state.player.currentDuration
+    },
+    totalDuration() {
+      return this.$store.state.player.totalDuration;
+    },
+    currentSeekPosition() {
+      const currentDuration = this.$store.state.player.currentDuration
+      const totalDuration = this.$store.state.player.totalDuration;
+      return (currentDuration * 100) / totalDuration;
+    },
     playerIcon() {
-      return this.$store.state.isPlaying ? pauseCircleOutline : playCircleOutline;
+      return this.isThisTrackPlaying ? pauseCircleOutline : playCircleOutline;
+    },
+    isThisTrackTheCurrentTrack() {
+      return this.$store.state.player.currentTrack && this.$store.state.player.currentTrack.id === this.track.id;
+    },
+    isThisTrackPlaying() {
+      return this.isThisTrackTheCurrentTrack && this.$store.state.player.isPlaying;
     },
   },
   methods: {
     playPauseButtonClicked() {
-      if(this.$store.state.player.isPlaying) {
-        // this track is now playing, thus the player must be paused
-        if(this.$store.state.player.currentTrack.id === this.trackId)
-          return this.$store.dispatch(ACTION_TYPES.PAUSE);
+      if(this.isThisTrackTheCurrentTrack) {
+        if(this.isThisTrackPlaying) {
+          this.$store.dispatch(ACTION_TYPES.PAUSE);
+        } else {
+          this.$store.dispatch(ACTION_TYPES.RESUME);
+        }
+      } else {
+        this.$store.dispatch(ACTION_TYPES.PLAY, this.track);
       }
-      // play this track (by adding it to the play queue)
-      this.$store.dispatch(ACTION_TYPES.ADD_TO_PLAY_QUEUE);
+    },
+    seekPositionChanged(ev: CustomEvent) {
+      this.$refs.range.$el.blur();
+      if(this.isRangeFocused)
+        this.$store.dispatch(ACTION_TYPES.SEEK, ev.detail.value);
     },
   },
 })
