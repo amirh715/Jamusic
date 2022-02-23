d810c9040669fa9b85e3b4f66ce460f600ab379f
Amirhossein Al <amirh715@gmail.com>
2022-02-24 02:33:57 +0330

    Add profile image field

--- src/classes/Profile/commands/EditProfileDTO.ts
+++ src/classes/Profile/commands/EditProfileDTO.ts
@@ -1,16 +1,21 @@
+import { dataURItoBlob } from "@/utils/DataURIToBlob";
+
 class EditProfileDTO {
 
   public name?: string;
   public email?: string;
+  public profileImage?: Blob;
   public removeProfileImage?: boolean;
 
   public constructor(_props: {
     name?: string,
     email?: string,
+    profileImage?: string,
     removeProfileImage?: boolean,
   }) {
     this.name = _props.name;
     this.email = _props.email;
+    this.profileImage = _props.profileImage && dataURItoBlob(_props.profileImage);
     this.removeProfileImage = _props.removeProfileImage;
   }
 
