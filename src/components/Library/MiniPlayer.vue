bb9b7f34eeda2da61ea26d7e041dfcec23e3e3df
Amirhossein Al <amirh715@gmail.com>
2022-02-24 02:47:45 +0330

    Add current seek position and fix play/pause icon

--- src/components/Library/MiniPlayer.vue
+++ src/components/Library/MiniPlayer.vue
@@ -1,17 +1,20 @@
 <template>
-  <div class="flex" style="height: 3.5rem; backdrop-filter: blur(20px);">
-    <div @click="$emit('titleTapped')" style="width: 80%">
-      <text-banner
-        :text="title"
-        :animate="false"
-      />
+  <div class="flex flex-column" style="height: 3.5rem; backdrop-filter: blur(20px);">
+    <div class="flex" style="height: 95%;">
+      <div @click="$emit('buttonTapped')" class="flex justify-content-center align-items-center" style="width: 20%">
+        <ion-icon
+          :icon="icon"
+          style="font-size: 2.7rem"
+        ></ion-icon>
+      </div>
+      <div @click="$emit('titleTapped')" class="flex flex-column" style="width: 80%;">
+        <text-banner :animate="currentTrack.title.length > 15">
+          <b>{{currentTrack.title}}</b>
+        </text-banner>
+        <small>{{currentTrack.artist.title}}</small>
+      </div>
     </div>
-    <div @click="$emit('buttonTapped')" class="flex justify-content-center align-items-center" style="width: 20%">
-      <ion-icon
-        :icon="icon"
-        style="font-size: 2.7rem"
-      ></ion-icon>
-    </div>
+    <div :style="{width: `${seekPosition}%`, height: '5%', border: '2px solid red'}"></div>
   </div>
 </template>
 
@@ -25,14 +28,17 @@ import {
 export default defineComponent({
   name: 'mini-player',
   emits: ['titleTapped', 'buttonTapped'],
-  props: {
-    isPlaying: Boolean,
-    title: String,
-    waiting: Boolean,
-  },
   computed: {
+    currentTrack() {
+      return this.$store.state.player.currentTrack;
+    },
+    seekPosition() {
+      const currentDuration = this.$store.state.player.currentDuration;
+      const totalDuration = this.$store.state.player.totalDuration;
+      return (currentDuration * 100) / totalDuration;
+    },
     icon() {
-      return this.isPlaying ? pauseCircleOutline : playCircleOutline;
+      return this.$store.state.player.isPlaying ? pauseCircleOutline : playCircleOutline;
     },
   },
 })
