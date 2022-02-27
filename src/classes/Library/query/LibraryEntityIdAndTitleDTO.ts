270f41eff1df7ecbc401b0a4132ebb38faa14ffa
Amirhossein Al <amirh715@gmail.com>
2022-02-27 22:24:33 +0330

    Add image processing properties

--- src/classes/Library/query/LibraryEntityIdAndTitleDTO.ts
+++ src/classes/Library/query/LibraryEntityIdAndTitleDTO.ts
@@ -2,13 +2,19 @@ class LibraryEntityIdAndTitle {
 
   public id: string;
   public title: string;
+  public image?: Blob;
+  public imageLoading?: boolean;
 
   public constructor(_props: {
     id: string,
     title: string,
+    image?: Blob,
+    imageLoading?: boolean,
   }) {
     this.id = _props.id;
     this.title = _props.title;
+    this.image = _props.image;
+    this.imageLoading = _props.imageLoading;
   }
 
 }
